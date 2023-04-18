eeaadd072810b3934e9d7bad6b2fbdbd46cc60ef
MyJstar <alclssha0301@naver.com>
2023-04-18 14:11:27 +0900

    // main class 실습 후

--- app/src/main/java/com/example/information/InformationActivity.java
+++ app/src/main/java/com/example/information/InformationActivity.java
@@ -1,8 +1,62 @@
 package com.example.information;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class InformationActivity extends AppCompatActivity {
 
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_information);
+
+        Intent it = getIntent();
+        String str= it.getStringExtra("id");
+
+        EditText editTextName = findViewById(R.id.editTextName);
+        EditText editTextAge = findViewById(R.id.editTextAge);
+        RadioButton radioMale = findViewById(R.id.radioMale);
+        RadioButton radioFemale = findViewById(R.id.radioFemale);
+        CheckBox cBInfo = findViewById(R.id.cBinfo);
+        CheckBox cBInfoAi = findViewById(R.id.cBAI);
+        CheckBox cBSecurity = findViewById(R.id.cBSecurity);
+
+        Button btnSend = findViewById(R.id.btnSend);
+
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent it = new Intent(); //  액티비티를 실행요청을 하지도 않고, 스택에 쌓이지 않으므로 매개변수가 필요없다.
+                it.putExtra("name", editTextName.getText().toString());
+                it.putExtra("age", editTextAge.getText().toString());
+                if( radioMale.isChecked()==true){
+                    it.putExtra("sex", "여자");
+                }else
+                    it.putExtra("sex", "남자");
+
+                String strLicense = "";
+                if (cBInfo.isChecked())
+                    strLicense += "\n 정보처리기사";
+                if (cBInfoAi.isChecked())
+                    strLicense += "\n 인공지능 ";
+                if (cBSecurity.isChecked())
+                    strLicense += "\n 네트워크보안";
+
+                it.putExtra("license", strLicense);
+                setResult(RESULT_OK, it);
+                finish();
+
+            }
+        });
+    }
 }
--- app/src/main/java/com/example/information/MainActivity.java
+++ app/src/main/java/com/example/information/MainActivity.java
@@ -5,14 +5,51 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView tVResult, tVResultLabel;
+    String str;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tVResult = findViewById(R.id.tVResult);
+        tVResultLabel = findViewById(R.id.tVResultLabel);
+
+        Button btnRequest =findViewById(R.id.btnRequest);
+        Button btnEnd = findViewById(R.id.btnEnd);
+
+        EditText editTextID = findViewById(R.id.editTextID);
+
+
+
+
+        btnRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                str = editTextID.getText().toString(); // 문자열로 형변환 해주는 메소드이다.
+                Intent it = new Intent(getApplicationContext(),InformationActivity.class);
+                it.putExtra("id", str);
+                startActivityForResult(it, 1);
+            }
+        });
+
+
+
+
+
+        btnEnd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
 
